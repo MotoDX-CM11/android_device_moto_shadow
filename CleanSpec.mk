9a91caa2188059c2d8a4a9e1311c336ea3bd4c91
mksy <maksik.boy@gmail.com>
2014-04-07 07:23:20 +0300

    little fix

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -44,7 +44,7 @@
 #$(call add-clean-step, find $(OUT_DIR) -type f -name "IGTalkSession*" -print0 | xargs -0 rm -f)
 #$(call add-clean-step, rm -rf $(PRODUCT_OUT)/data/*)
 
-$(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/DefyParts_intermediates)
+$(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/DXParts_intermediates)
 
 # For the sensors lib
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/SHARED_LIBRARIES/sensors.shadow_intermediates)
